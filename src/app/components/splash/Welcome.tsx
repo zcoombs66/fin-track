a6b24b4468a0fca6389e479b9f7166d9f3f6cecf
PericlesLelos <pal45579@uga.edu>
2025-04-13 18:58:37 -0400

    added animations to Welcome.tsx and welcome.css

--- src/app/components/splash/Welcome.tsx
+++ src/app/components/splash/Welcome.tsx
@@ -12,11 +12,11 @@ export default function Welcome() {
     const router = useRouter();
     return (
         <div className='welcome-container'>
-            <h1>Fin-Track</h1>
-            <h2>Track your expenses effortlessly</h2>
-            <div className='login-container'>
-                <button onClick={() => router.push('/signin')}>Sign In</button>
-                <button onClick={() => router.push('/signup')}>Register</button>
+            <h1 className='title-container'>Fin-Track</h1>
+            <h2 className='h-2-container'>Track your expenses effortlessly</h2>
+            <div className='login-button-container'>
+                <button onClick={() => router.push('/signin')} className='login-button'>Sign In</button>
+                <button onClick={() => router.push('/signup')} className='login-button'>Register</button>
             </div>
             <div className='image-container'>
                 <Image 
